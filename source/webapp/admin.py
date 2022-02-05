84923136986abbca080b907f1b23b96f8662880d
mtashtanovski <mtashtanovski@gmail.com>
2022-02-05 14:24:04 +0600

    Созданы модели Опроса и вариантов ответа на них, сделана миграция. Зарегил модели в админке и добавил данные для работы с проектом. Дамп данных.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created']
+    list_filter = ['created']
+    search_fields = ['question', 'created']
+    fields = ['question']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=100, null=False, blank=False, verbose_name='Опрос')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f"{self.pk}: {self.question}"
+
+    class Meta:
+        db_table = 'questions'
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Choice(models.Model):
+    answer = models.CharField(max_length=30, null=False, blank=False, verbose_name='Вариант ответа')
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, related_name='choices', verbose_name='Опрос')
+
+    def __str__(self):
+        return f"{self.answer}"
+
+    class Meta:
+        db_table = 'choices'
+        verbose_name = 'Вариант'
+        verbose_name_plural = 'Варианты'
