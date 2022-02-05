7b29cdd000def027be24453773f8bf1af6c1fe9f
mtashtanovski <mtashtanovski@gmail.com>
2022-02-05 15:55:27 +0600

    Создана главная страница с списком опросов. Добавлены в урл. Создан базовый шаблон HTML. Добавлен частичный шаблон для пагинации на главной странице.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+
+
+from webapp.views import (IndexView,
+                          )
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index')
+]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView, FormView, ListView
 
 # Create your views here.
+from webapp.models import Poll, Choice
+
+
+class IndexView(ListView):
+    model = Poll
+    context_object_name = 'polls'
+    template_name = 'index.html'
+    paginate_by = 5
+    paginate_orphans = 0
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.order_by("-created")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        print(context)
+
+        return context
+
